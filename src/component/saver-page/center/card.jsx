26f6b22549ef2f46580b6c3380c641e78a6a1156
xdt <119037795+dabeiteec@users.noreply.github.com>
2024-10-21 01:27:11 +0400

    saver page

--- src/component/saver-page/center/card.jsx
+++ src/component/saver-page/center/card.jsx
@@ -1,14 +1,23 @@
 import "../../global/css/button.scss";
+import "../../global/global.scss";
+import PropTypes from "prop-types";
 
-const Card = (cardImg, cardAlt, cardText) => {
+Card.propTypes = {
+    imgBlue: PropTypes.string.isRequired,
+    altBlue: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+};
+
+const Card = ({ description, altBlue, imgBlue }) => {
     return (
         <div className="flex">
-            <img alt={cardAlt} src={cardImg} />
+            <img alt={altBlue} src={imgBlue} />
             <div className="column">
-                <span>{cardText}</span>
+                <span className="glow-text">{description}</span>
                 <button className="gradient-button">Сохранить</button>
             </div>
         </div>
     );
 };
+
 export default Card;
--- src/component/saver-page/center/main-items.jsx
+++ src/component/saver-page/center/main-items.jsx
@@ -1,4 +1,76 @@
+import blueFilm from "../../../assets/blue_film.png";
+import blueSeries from "../../../assets/blue_series.png";
+import blueAnime from "../../../assets/blue_anime.png";
+import blueMult from "../../../assets/blue_mult.png";
+import blueGame from "../../../assets/blue_game.png";
+import pinkFilm from "../../../assets/pink_film.png";
+import pinkSeries from "../../../assets/pink_film.png";
+import pinkAnime from "../../../assets/pink_film.png";
+import pinkMult from "../../../assets/pink_film.png";
+import pinkGame from "../../../assets/pink_film.png";
+import Card from "./card";
+import "../../global/global.scss";
+
 const MainItems = () => {
-    return <div></div>;
+    // TODO
+    // this is a temporary imitation of the database
+    const dataBase = {
+        film: {
+            imgBlue: blueFilm,
+            imgPink: pinkFilm,
+            altBlue: "Blue Img for Film",
+            altPink: "Pink Img for Film",
+            description:
+                "Сохрани любимые фильмы и возвращайся к ним, когда захочешь! Все твои впечатления — в одном месте.",
+        },
+        Series: {
+            imgBlue: blueSeries,
+            imgPink: pinkAnime,
+            altBlue: "Blue Img for Series",
+            altPink: "Pink Img for Series",
+            description:
+                "Залипли на сериал? Сохраняйте его в вашей коллекции и возвращайтесь за новыми впечатлениями!",
+        },
+        Anime: {
+            imgBlue: blueAnime,
+            imgPink: pinkSeries,
+            altBlue: "Blue Img for Anime",
+            altPink: "Pink Img for Anime",
+            description:
+                "Наслаждаетесь аниме? Сохраняйте в коллекцию и исследуйте новые горизонты!",
+        },
+        Cartoon: {
+            imgBlue: blueMult,
+            imgPink: pinkMult,
+            altBlue: "Blue Img for Cartoon",
+            altPink: "Pink Img for Cartoon",
+            description:
+                "Сохраняйте любимые мультфильмы и создавайте свою уникальную коллекцию!",
+        },
+        Game: {
+            imgBlue: blueGame,
+            imgPink: pinkGame,
+            altBlue: "Blue Img for Game",
+            altPink: "Pink Img for Game",
+            description:
+                "Сохраняйте свои любимые игры и получайте максимум удовольствия!",
+        },
+    };
+
+    return (
+        <section className="column">
+            {Object.keys(dataBase).map((key) => (
+                <Card
+                    key={key}
+                    imgBlue={dataBase[key].imgBlue}
+                    // imgPink={dataBase[key].imgPink}
+                    altBlue={dataBase[key].altBlue}
+                    // altPink={dataBase[key].altPink}
+                    description={dataBase[key].description}
+                />
+            ))}
+        </section>
+    );
 };
+
 export default MainItems;
